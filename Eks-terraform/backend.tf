9b8098d8cd83033c0e09bc3170795383394fbaa8
mamathasuram <154117823+mamathasuram@users.noreply.github.com>
2024-02-15 16:43:28 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myempapp2023" # Replace with your actual S3 bucket name
+    bucket = "lappybuckets3" # Replace with your actual S3 bucket name
     key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
